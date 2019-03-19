a74c119061e1b72451071606fed5d6b67fb7ed81
chengyi <cheng@163.com>
2019-03-19 22:58:54 +0800

    contact 最后跳转待修复

--- application/index/controller/Contact.php
+++ application/index/controller/Contact.php
@@ -9,7 +9,7 @@
 namespace  app\index\controller;
 use think\Db;
 use app\index\controller\Common;
-
+use think\Request;
 class Contact extends Common{
 
     //联系我们
@@ -19,19 +19,20 @@ class Contact extends Common{
 
     //接受客户提交信息 todo 表单需要验证 需要优化
     public function contact_info(){
-        $data = input('post.');
-        $insert['name']  = $data['your-name'];
-        $insert['email']  = $data['your-email'];
-        $insert['phone']  = $data['your-phone'];
-        $insert['message']  = $data['your-message'];
-        if(empty($data['your-name'])){
-           return  $this->result(['status'=>'validation_failed'],'400','error','json');
+        if($this->request->isPost()){
+           // $data = input('post.');
+            $insert['name']  = input('post.name');
+            $insert['email']  = input('post.email');
+            $insert['phone']  = input('post.phone');
+            $insert['message']  =input('post.message');
+            $res = Db::name('contact')->data($insert)->insert();
+            if($res){
+                $this->result(['status'=>'mail_sent'],'200','mail_sent','json');
+             }else{
+                $this->result('','400','error','json');
+             }
+           }
         }
-        $res = Db::name('contact')->data($insert)->insert();
-        if($res){
-           $this->result(['status'=>'mail_sent'],'200','mail_sent','json');
-        }
-      return false;
-    }
+
 
 }
